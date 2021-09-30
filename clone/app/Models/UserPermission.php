ef5e0b0f9a5a95786e99fdb4988bbd428cb1b870
Michael Engelbrecht <michaelndani@gmail.com>
2021-09-30 17:36:58 +1000

    Add headers:
    /**
     * This file is cloned / force-published from the "auth-laravel-v1" composer package.
     *    WARNING: Local modifications will be overwritten when the package is updated.
     *             See https://github.com/faithfm/auth-laravel-v1 for more details.
     */

--- clone/app/Models/UserPermission.php
+++ clone/app/Models/UserPermission.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * This file is cloned / force-published from the "auth-laravel-v1" composer package.
+ *    WARNING: Local modifications will be overwritten when the package is updated.
+ *             See https://github.com/faithfm/auth-laravel-v1 for more details.
+ */
 
 namespace App\Models;
 
